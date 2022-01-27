3280dcc4e9ebd5abc1f2c7f9fcc9792ff71922c3
Surafel Melese <surafel.melese@CNETERP.COM>
2022-01-27 17:40:30 +0300

    halfway through the binance coin icon

--- lib/painters/binance_coin.dart
+++ lib/painters/binance_coin.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class BinanceCoinIconPainter extends CustomPainter {
+  late Size iconSize;
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    iconSize = size;
     double overallOffsetRatio = 0.25;
-    double gapRatio = 0.1;
-    double howThickRatio = 0.15;
+    double gapRatio = 0.15;
+    double howThickRatio = 0.2;
     const Color iconBackgroundColor = Color.fromRGBO(243, 186, 44, 1);
     double thickness = size.width * howThickRatio;
     double gap = size.width * gapRatio;
@@ -26,14 +27,53 @@ class BinanceCoinIconPainter extends CustomPainter {
     Offset K = Offset(size.width * overallOffsetRatio, size.height * 0.5);
     Offset H = Offset(K.dx + thickness / sqrt(2), K.dy - thickness / sqrt(2));
     double angleLHE = atan(gap / thickness);
-    Offset C = Offset(H.dx + gap * cos(angleLHE), H.dy - gap * sin(angleLHE));
+    Offset C = Offset(H.dx + gap * cos(pi / 2 - angleLHE),
+        H.dy - gap * sin(pi / 2 - angleLHE));
     Offset L = Offset(K.dx + thickness * sqrt(2), K.dy);
     Offset E = Offset(L.dx + gap * cos(pi / 2 - angleLHE),
         L.dy - gap * sin(pi / 2 - angleLHE));
     Offset M = Offset(L.dx + gap * sqrt(2), K.dy);
     Offset F = Offset(A.dx, B.dy + gap * sqrt(2));
+    Offset D = reflectYAxis(C);
+    Offset G = reflectYAxis(E);
+
+    Path topArrowPath = Path()
+      ..addPolygon([
+        A,
+        C,
+        E,
+        B, /*G, D*/
+      ], false);
+    brush.color = Colors.white;
+    canvas.drawPath(topArrowPath, brush);
     //the rest of the points can be obtained by reflecting through y=x, y=0 and x=0
   }
+
+  reflectYAxis(Offset point) {
+    Offset cartesianPoint = toCartesianCoordinate(point);
+    Offset cartesianValue = Offset(-cartesianPoint.dx, cartesianPoint.dy);
+    return toDartCoordinate(cartesianValue);
+  }
+
+  reflectXAxis(Offset point) {
+    Offset cartesianPoint = toCartesianCoordinate(point);
+    Offset cartesianValue = Offset(cartesianPoint.dx, -cartesianPoint.dy);
+    return toDartCoordinate(cartesianValue);
+  }
+
+  reflectYEqualsX(Offset point) {
+    Offset cartesianPoint = toCartesianCoordinate(point);
+    Offset cartesianValue = Offset(cartesianPoint.dy, cartesianPoint.dx);
+    return toDartCoordinate(cartesianValue);
+  }
+
+  toDartCoordinate(Offset cartesianPoint) => Offset(
+      cartesianPoint.dx + iconSize.width * 0.5,
+      cartesianPoint.dy + iconSize.height * 0.5);
+
+  toCartesianCoordinate(Offset dartianPoint) => Offset(
+      dartianPoint.dx - iconSize.width * 0.5,
+      dartianPoint.dy - iconSize.height * 0.5);
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
